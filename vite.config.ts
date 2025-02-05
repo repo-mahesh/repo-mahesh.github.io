00ee66521ad4ce8b7f73902fd0e7d8206a2e26fe
Work <work@Manishas-MacBook.local>
2025-02-05 19:25:22 +0530

    into frontend

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/repo-mahesh/",
+  // base: "/repo-mahesh/",
+  base: "/",
   assetsInclude: ["**/*.svg"], // Add this line to handle SVG files
   build: {
     outDir: 'dist',
